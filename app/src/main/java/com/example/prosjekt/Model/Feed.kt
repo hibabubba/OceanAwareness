93778d1ea65f1eee51771a77f22462ef8eb5d1fa
rokast <rokast@uio.no>
2020-04-21 22:03:55 +0200

    Glemte å adde noen filer

---
+++ app/src/main/java/com/example/prosjekt/Model/Feed.kt
@@ -0,0 +1,8 @@
+package com.example.prosjektin2000
+
+data class Feed (
+    val url:String,
+    val title:String,
+    val link:String,
+    val description:String
+)
---
+++ app/src/main/java/com/example/prosjekt/Model/Item.kt
@@ -0,0 +1,6 @@
+package com.example.prosjektin2000
+
+
+data class Item(val title:String, val pubDate :String, val link:String,val guide:String,
+                val author:String, val thumbnail:String, val description:String, val content:String,
+                val enclosure:Object,val categories:List<String>)
