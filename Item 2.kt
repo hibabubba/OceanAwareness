cf736f9ac337f78ced586482fc4e2713e77f3950
Rokas Magnus Trimbelis <rokast@student.matnat.uio.no>
2020-04-23 12:06:33 +0200

    Delete Item 2.kt

--- Item 2.kt
+++
@@ -1,5 +0,0 @@
-package com.example.prosjektin2000
-
-data class Item(val title:String, val pubDate :String, val link:String,val guide:String,
-                val author:String, val thumbnail:String, val description:String, val content:String,
-                val enclosure:Object,val categories:List<String>)
